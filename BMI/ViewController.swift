44f9b4d1538b359032039025a14b27b9ae0e6fe5
ksg0605 <ksg0605@gmail.com>
2023-08-31 17:33:11 +0900

    Initial Commit

---
+++ BMI/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMI
+//
+//  Created by 김선규 on 2023/08/31.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
